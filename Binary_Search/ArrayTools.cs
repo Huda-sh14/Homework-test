5258b33d4e8bf48f59ec4a4b126794fb7dd9e410
Huda Shakir <huda.f.shakir@gmail.com>
2023-10-20 12:33:01 +0300

    Huda Shakir

--- Binary_Search/ArrayTools.cs
+++ Binary_Search/ArrayTools.cs
@@ -10,28 +10,71 @@ namespace Binary_Search
     {
         public static void FillArray(ref int[] arr)
         {
-            // TODO: fill array incremantaly 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
         }
 
 
         public static int LinearSearchI(int[] arr, int key)
         {
-            // TODO: write linear search code in iteration
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         public static int LinearSearchR(int[] arr, int key,int idx)
         {
-            // TODO: write linear search code in recursive
+            if (arr.Length == idx)
+                return -1;
+            if (arr[idx] == key)
+                return key;
+            return LinearSearchR(arr, key, idx + 1);
         }
 
         public static int BinarySearchR(int[] arr, int key, int start_idx, int end_idx)
         {
-            // TODO: write binary search code in recursive
+            int middle_idx = (start_idx + end_idx) / 2;
+            if (arr[middle_idx] == key)
+            {
+                return middle_idx;
+            }
+            if (start_idx > end_idx)
+            {
+                return -1;
+            }
+            if (arr[middle_idx] > key)
+            {
+                return BinarySearchR(arr, key, start_idx, middle_idx - 1);
+            }
+            return BinarySearchR(arr, key, middle_idx + 1, end_idx);
         }
 
         public static int BinarySearchI(int[] arr, int key)
         {
-            // TODO: write binary search code in iterative
+            int start_idx = 0;
+            int end_idx = arr.Length - 1;
+            while (start_idx <= end_idx)
+            {
+                int middle_idx = (start_idx + end_idx) / 2;
+                if (arr[middle_idx] == key)
+                {
+                    return key;
+                }
+                if (arr[middle_idx] < key)
+                {
+                    start_idx = middle_idx + 1;
+                }
+                else
+                    end_idx = middle_idx - 1;
+            }
+            return -1;
         }
     }
 }
